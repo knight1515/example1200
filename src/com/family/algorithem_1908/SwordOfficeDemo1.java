40b69fd8119a04e1bd0f28f422c94e24a329f89a
galen17 <daguozb@163.com>
2019-09-24 14:45:08 +0800

    两个栈实现队列和斐波那契数列算法

--- src/com/family/algorithem_1908/SwordOfficeDemo1.java
+++ src/com/family/algorithem_1908/SwordOfficeDemo1.java
@@ -1,6 +1,12 @@
 package com.family.algorithem_1908;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import org.apache.jasper.xmlparser.TreeNode;
+import org.junit.Test;
 
 /**
  * •3. 数组中重复的数字
@@ -138,11 +144,126 @@ public class SwordOfficeDemo1 {
         return str.toString();
     }
     
+    /**
+     * 9. 用两个栈实现队列
+     * 栈是先进后出，队列是先进先出。
+     * 两个栈的颠倒能实现第二个栈弹出pop的是先进的。
+     */
+    Stack<Integer> in = new Stack<Integer>();
+    Stack<Integer> out = new Stack<Integer>();
     
+    //用栈in来完成队列的push功能
+    public void pust(int node) {
+        in.push(node);
+    }
     
-
+    //用out实现pop功能
+    public int pop() throws Exception{
+        if(out.isEmpty()) {
+            while(!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        if (out.isEmpty()) {
+            throw new Exception("queue is empty!");
+        }
+        return out.pop();
+    }
+    //--------------------------------9. 用两个栈实现队列-结束---------------------------------
     
+    /**
+     * 10.1 斐波那契数列的第n项
+     * 
+     * @author: gzb
+     * @date  : 2019年9月24日 下午2:11:57
+     */
+    @Test
+    public void testFibonacci() {
+        SwordOfficeDemo1 so = new SwordOfficeDemo1();
+        int fibonacci = so.Fibonacci(10);
+        System.out.println(fibonacci);
+    }
+    /**
+     * 如果使用递归求解，会重复计算一些子问题。例如，计算 f(4) 需要计算 f(3) 和 f(2)，计算 f(3) 需要计算 f(2) 和 f(1)，可以看到 f(2) 被重复计算了。
+     * 递归是将一个问题划分成多个子问题求解，动态规划也是如此，但是动态规划会把子问题的解缓存起来，从而避免重复求解子问题。
+     * 
+     * @author: gzb
+     * @date  : 2019年9月24日 下午2:13:12
+     * @param n
+     * @return
+     */
+    public int Fibonacci(int n) {
+        if(n<=1) {
+            return n;
+        }
+        int[] fib = new int[n+1];
+        fib[1] = 1;
+        for(int i=2;i<=n;i++) {
+            fib[i] = fib[i-1] +fib[i-2];
+        }
+        return fib[n];
+    }
+    
+    /**
+     * 考虑到第 i 项只与第 i-1 和第 i-2 项有关，因此只需要存储前两项的值就能求解第 i 项，从而将空间复杂度由 O(N) 降低为 O(1)。
+     * 
+     * @author: gzb
+     * @date  : 2019年9月24日 下午2:30:49
+     * @param n
+     * @return
+     */
+    public int Fibonacci2(int n) {
+        if (n<=1) {
+            return n;
+        }
+        int pre2=0,pre1=1;
+        int fib=0;
+        for(int i=2;i<=n;i++) {
+            fib = pre1+pre2;
+            pre1=pre2;
+            pre2=fib;
+        }
+        return fib;
+    }
 }
+
+
+/**
+ * 10.1 斐波那契数列的第n项
+ * 由于待求解的 n 小于 40，因此可以将前 40 项的结果先进行计算，之后就能以 O(1) 时间复杂度得到第 n 项的值。
+ * 
+ * @author: gzb
+ * @date  : 2019年9月24日 下午2:40:26
+ *
+ */
+class Solution {
+
+    private int[] fib = new int[40];
+
+    public Solution() {
+        fib[1] = 1;
+        for (int i = 2; i < fib.length; i++)
+            fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    public int Fibonacci(int n) {
+        return fib[n];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

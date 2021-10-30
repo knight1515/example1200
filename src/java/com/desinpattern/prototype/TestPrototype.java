d9649ed56429cc36cee25adb628c34b6edcb5927
shangjiang <daguozb@163.com>
2021-10-30 22:20:40 +0800

    原型模式练习

---
+++ src/java/com/desinpattern/prototype/TestPrototype.java
@@ -0,0 +1,16 @@
+package com.desinpattern.prototype;
+
+public class TestPrototype {
+    public static void main(String[] args) {
+        Manager manager = new Manager();
+        UnderlinePen upen = new UnderlinePen('~');
+        MessageBox mbox = new MessageBox('*');
+        MessageBox sbox = new MessageBox('/');
+        manager.register("strong message", upen);
+        manager.register("warning box", mbox);
+        manager.register("slash box", sbox);
+
+        Product p1 = manager.create("strong message");
+        p1.use("Hello, world");
+    }
+}
---
+++ src/java/com/desinpattern/prototype/UnderlinePen.java
@@ -0,0 +1,31 @@
+package com.desinpattern.prototype;
+
+public class UnderlinePen implements Product {
+    private char ulchar;
+
+    public UnderlinePen(char ulchar) {
+        this.ulchar = ulchar;
+    }
+
+    @Override
+    public void use(String s) {
+        int length = s.getBytes().length;
+        System.out.println("\"" + s + "\"");
+        System.out.print(" ");
+        for (int i = 0; i < length; i++) {
+            System.out.println(ulchar);
+        }
+        System.out.println("");
+    }
+
+    @Override
+    public Product createClont() {
+        Product p = null;
+        try {
+            p = (Product) clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+}
